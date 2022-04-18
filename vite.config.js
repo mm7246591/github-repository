6c82fa84b7c305a73998162099e4b422c614e339
Yan <mm7246591@yahoo.com.tw>
2022-04-19 00:41:33 +0800

    Update

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 export default defineConfig({
-    undefined,
     plugins: [vue()],
-    server: {
-        undefined,
-        host: "0.0 .0 .0",
-        port: 3000,
-        // 是否开启 https
-        https: false,
-    },
+    publicDir: "public",
     base: "./",
 });
